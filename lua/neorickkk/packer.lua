b9d2fdad4154732d3f5021d3353d36f958fa750d
mohamidsaiid <m.saeed2003@hotmail.com>
2025-05-05 19:44:58 +0300

    added workspace plugin

--- lua/neorickkk/packer.lua
+++ lua/neorickkk/packer.lua
@@ -46,6 +46,14 @@ use {
     'folke/trouble.nvim',              -- diagnostics UI
     'nvim-lualine/lualine.nvim',       -- statusline (optional)
 }
-
-
+use {
+  "ahmedkhalf/project.nvim",
+  config = function()
+    require("project_nvim").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
 end)
